9d361489cc38e4eae6115a97182ffebdcc199d74
Juan Esteban <cardonaospinajuanesteban@gmail.com>
2022-11-08 21:02:28 -0500

    fix image

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const express = require("express");
 const app = express();
 const port = 80;
-const phrases = [
-  "Get ready to be inspired…",
-  "See rejection as redirection.",
-  "There is beauty in simplicity.",
-  "You can’t be late until you show up.",
-  "Maybe life is testing you. Don’t give up.",
-  "Impossible is just an opinion.",
-  "Alone or not you gonna walk forward.",
+
+//motor 
+// app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+
+// app.get('/', (req, res) => {
+//   res.render("index", {Frase, id})
+// })
+
+const neas = [
+  {id: "aaaaa" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "5453", Frase:""},
+  {id: "bbb" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "453543", Frase:""},
+  {id: "ccc" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "453543", Frase:""},
+  {id: "hhh" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "345354", Frase:""},
+  {id: "jjj" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "45345", Frase:""},
+  {id: "rrr" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "547", Frase:""},
+  {id: "" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "838", Frase:""},
+  {id: "" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "", Frase:""},
+  {id: "" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "", Frase:""},
+  {id: "" , Nombre: "", Altura: " " , Habilidad: "", Imagen: "", Frase:""}
 ];
-app.get("/", (req, res) => {
-  const number = Math.floor(Math.random() * 7);
-  res.send(phrases[number]);
+
+app.get("/info", (req, res) => {
+  const number = Math.floor(Math.random() * 10);
+  res.send(neas[number]);
 });
+
+app.get("/imagenes", (req, res) => {
+  const nea = Math.floor(Math.random() * 10);
+  res.send(Imagen[nea]);
+});
+
+app.get("/image", (req, res) => {
+   const nea = Math.floor(Math.random() * 10);
+   const id = neas[nea].id;
+   const Nombre = neas[nea].Nombre;
+   const Frase = neas[nea].Frase;
+   const Imagen = neas[nea].Imagen;
+  res.sendFile( __dirname + '/index.html');
+  res.send(id);
+  res.send(Nombre);
+  res.send(Frase);
+  res.send(Imagen);
+});
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
